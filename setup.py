e786ba7cc8d6e59b57c3b877c385c37f7278c475
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2017-01-25 16:54:46 -0300

    version number updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pykhipu',
-    version='0.1.1',
+    version='0.1.2',
     description='Wrapper for the Khipu payment service API v2.0',
     long_description=readme(),
     classifiers=[
