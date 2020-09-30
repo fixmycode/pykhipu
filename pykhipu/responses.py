c954b45cfb4570fd4cc2355c1a73e9fc3851fcd3
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2020-09-30 00:25:50 -0300

    soluciona error en caso de mensaje vacio

--- pykhipu/responses.py
+++ pykhipu/responses.py
@@ -537,7 +537,7 @@ class SuccessResponse(BaseResponse):
 
     @classmethod
     def from_data(cls, data):
-        return cls(data['message'])
+        return cls(data.get('message'))
 
     @property
     def message(self):
